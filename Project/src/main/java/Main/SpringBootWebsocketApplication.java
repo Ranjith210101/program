aabf235644704ee5795f205966808a55fcf32b67
Ranjith210101 <114902667+Ranjith210101@users.noreply.github.com>
2023-01-12 20:53:29 +0530

    Add files via upload
    Chat

---
+++ Project/src/main/java/Main/SpringBootWebsocketApplication.java
@@ -0,0 +1,13 @@
+package Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringBootWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebsocketApplication.class, args);
+    }
+}
